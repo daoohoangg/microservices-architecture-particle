c6321b2f1ad8c3bb7c7b2d9b7308da6a33ac5a48
daoohoangg <daohoang2911@gmail.com>
2025-04-11 14:54:55 +0700

    smart-contract-create

---
+++ smart-contract-hardhat/scripts/deploy.js
@@ -0,0 +1,13 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory("MyContract");
+  const contract = await Contract.deploy();
+
+  console.log(`Contract deployed to: ${contract.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
